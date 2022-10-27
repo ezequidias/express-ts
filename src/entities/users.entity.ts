57cda8defbe95a086a3f0015f7c50553541d8b94
Ezequiel Dias <ezequidias@outlook.com>
2022-10-27 19:36:50 -0300

    Update entity to users

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { User } from '@interfaces/users.interface';
 
-@Entity()
+@Entity({ name: 'users' })
 export class UserEntity extends BaseEntity implements User {
   @PrimaryGeneratedColumn()
   id: number;
@@ -17,10 +17,10 @@ export class UserEntity extends BaseEntity implements User {
   password: string;
 
   @Column()
-  @CreateDateColumn()
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
   @Column()
-  @UpdateDateColumn()
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
